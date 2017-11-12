e24c7d72e9caa7cbae896538079cb3934112ef90
Olaf Kuusik <olafkuusik@gmail.com>
2017-11-12 20:23:06 +0200

    Ülesanne 7
    Calculator Controller

---
+++ OlafKuusik/Controllers/CalcController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OlafKuusik.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Calc()
+        {
+            return View();
+        }
+
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Substract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("Pick any other number than zero");
+                return 0;
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+    }
+}
