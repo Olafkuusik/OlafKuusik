79aa037b952e26ca1966f715a2d0c347efff4e3d
Olaf Kuusik <olafkuusik@gmail.com>
2017-11-12 15:38:49 +0200

    Ülesanne 2
    Added 2 Model classes

---
+++ OlafKuusik/Models/VideoGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OlafKuusik.Models
+{
+    public class VideoGame
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ItemsInStock { get; set; }
+        public DateTime ReleasedDate { get; set; }
+    }
+}
