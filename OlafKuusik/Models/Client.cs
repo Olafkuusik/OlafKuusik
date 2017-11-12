497e2286295d8f3a79153f9ada7eb8f36485a3ec
Olaf Kuusik <olafkuusik@gmail.com>
2017-11-12 17:57:49 +0200

    Ülesanne 4
    Scaffolding done, added 2 clients and 2 games via CRUD create menu on
    the localdev testsite

--- OlafKuusik/Models/Client.cs
+++ OlafKuusik/Models/Client.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +10,18 @@ namespace OlafKuusik.Models
     public class Client
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
         public string Name { get; set; }
+
+        [Range(16,80)]
         public int Age { get; set; }
+
         public DateTime JoiningDate { get; set; }
+    }
+
+    public class ClientDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
     }
 }
--- OlafKuusik/Models/VideoGame.cs
+++ OlafKuusik/Models/VideoGame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -11,5 +12,9 @@ namespace OlafKuusik.Models
         public string Name { get; set; }
         public int ItemsInStock { get; set; }
         public DateTime ReleasedDate { get; set; }
+    }
+    public class VideoGameDBContext : DbContext
+    {
+        public DbSet<VideoGame> VideoGames { get; set; }
     }
 }
